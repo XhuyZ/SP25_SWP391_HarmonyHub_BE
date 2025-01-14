c17e839a5a7e6af4aa2ffd5f8f6d9f67d769a4e7
predrir <predrirvn@gmail.com>
2025-01-14 09:23:00 +0700

    Added Cloudinary for uploading images

---
+++ API/Controllers/CloudinaryController.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Constants;
+using Domain.DTOs.Common;
+using Domain.DTOs.Requests;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Exceptions;
+using Service.Implementations;
+using Service.Interfaces;
+
+namespace API.Controllers;
+
+[Route("api/cloudinary")]
+[ApiController]
+
+public class CloudinaryController : ApiBaseController
+{
+    private readonly ICloudinaryService _cloudinaryService;
+    public CloudinaryController(ICloudinaryService cloudinaryService)
+    {
+        _cloudinaryService = cloudinaryService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        try
+        {
+            var result = await _cloudinaryService.UploadFile(file);
+            return Ok(result);
+        }
+        catch (ServiceException e)
+        {
+            return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest, e.Message));
+        }
+    }
+}
---
+++ Service/Implementations/CloudinaryService.cs
@@ -0,0 +1,55 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Domain.Entities;
+using Google.Protobuf.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Service.Interfaces;
+using Service.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Implementations
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary _cloudinary;
+        private readonly CloudinarySettings _cloudinarySetting;
+
+        public CloudinaryService(IOptions<CloudinarySettings> cloudinarySetting)
+        {
+            _cloudinarySetting = cloudinarySetting.Value;
+            var account = new CloudinaryDotNet.Account(
+                        _cloudinarySetting.Cloud,
+                        _cloudinarySetting.ApiKey,
+                        _cloudinarySetting.ApiSecret);
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+
+        public async Task<string> UploadFile(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("File is empty or null");
+            var uploadResult = new ImageUploadResult();
+
+            await using var stream = file.OpenReadStream();
+            var uploadParams = new ImageUploadParams
+            {
+                File = new FileDescription(file.FileName, stream),
+                Transformation = new Transformation().Quality(80).Crop("limit")
+            };
+
+            uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+            if (uploadResult.Error != null)
+                throw new Exception(uploadResult.Error.Message);
+
+            return uploadResult.SecureUrl.ToString();
+        }
+    }
+}
