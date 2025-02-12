4d7707100d8b705f81e32b53972f6c9d1bb8cb22
predrir <predrirvn@gmail.com>
2025-02-12 14:47:26 +0700

    Added Requests for creating quiz

---
+++ Domain/DTOs/Requests/CreateQuizQuestionRequest.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.DTOs.Requests
+{
+    public class CreateQuizQuestionRequest
+    {
+        public int QuizId { get; set; }
+        public Quiz Quiz { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
