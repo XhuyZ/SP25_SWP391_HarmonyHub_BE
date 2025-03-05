b5b7826551365350b59dea129312844b6279676a
Minh Dinh <macotontai123@gmail.com>
2025-03-06 03:21:30 +0700

    feat(vnpay): refactor vnpay service

--- Domain/DTOs/Requests/CreatePaymentRequest.cs
+++ Domain/DTOs/Requests/CreatePaymentRequest.cs
@@ -1,21 +1,7 @@
 ﻿namespace Domain.DTO.Request;
+
 public class CreatePaymentRequest
 {
-    public long Amount { get; set; }
-    public string OrderInfo { get; set; }
-}
-
-public class CreatePaymentUserRequest
-{
-    public int UserId { get; set; }
-    public long Amount { get; set; }
-    public string OrderInfo { get; set; }
-}
-
-public class TransferPaymentUserRequest
-{
-    public int UserId { get; set; }
-    public int ReceiverId { get; set; }
     public long Amount { get; set; }
     public string OrderInfo { get; set; }
 }
---
+++ Service/Helper/VnPayUtils.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace VNPAY_CS_ASPX;
+
+public class VnPayUtils
+{
+    public static String HmacSHA512(string key, String inputData)
+    {
+        var hash = new StringBuilder();
+        byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+        byte[] inputBytes = Encoding.UTF8.GetBytes(inputData);
+        using (var hmac = new HMACSHA512(keyBytes))
+        {
+            byte[] hashValue = hmac.ComputeHash(inputBytes);
+            foreach (var theByte in hashValue)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+        }
+
+        return hash.ToString();
+    }
+
+    public static string GetIpAddress(IHttpContextAccessor httpContextAccessor)
+    {
+        string ipAddress;
+        try
+        {
+            ipAddress = httpContextAccessor.HttpContext.Request.Headers["HTTP_X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(ipAddress) || (ipAddress.ToLower() == "unknown") || ipAddress.Length > 45)
+                ipAddress = httpContextAccessor.HttpContext.Connection.RemoteIpAddress?.ToString();
+        }
+        catch (Exception ex)
+        {
+            ipAddress = "Invalid IP:" + ex.Message;
+        }
+
+        return ipAddress;
+    }
+}
