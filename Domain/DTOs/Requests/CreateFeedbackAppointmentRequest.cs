9261b5ccf5227095e6a12f124ad8e48496b196ca
Minh Dinh <macotontai123@gmail.com>
2025-03-16 02:17:13 +0700

    refactor(*): refactor code in APIs, DTOs, remove unused imports, add qualification repository

--- Domain/DTOs/Requests/CreateFeedbackAppointmentRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.DTOs.Requests
-{
-    public class CreateFeedbackAppointmentRequest
-    {
-        public double? FeedbackRating { get; set; }
-        public string? FeedbackContent { get; set; }
-        public DateTime? FeedbackDate { get; set; }
-    }
-}
