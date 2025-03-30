aeaab5964c533444737076026881b1024f40e8d5
Minh Dinh <macotontai123@gmail.com>
2025-03-30 16:40:47 +0700

    refactor(naming): add `/api` naming for availability

--- API/Controllers/AvailabilityController.cs
+++ API/Controllers/AvailabilityController.cs
@@ -2,65 +2,63 @@
 using Domain.DTOs.Common;
 using Domain.DTOs.Requests;
 using Microsoft.AspNetCore.Mvc;
-using Org.BouncyCastle.Asn1.Ocsp;
 using Service.Exceptions;
-using Service.Implementations;
 using Service.Interfaces;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+[ApiController]
+public class AvailabilityController : ApiBaseController
 {
-    public class AvailabilityController : Controller
+    private readonly IAvailabilityService _availabilityService;
+
+    public AvailabilityController(IAvailabilityService availabilityService)
     {
-        private readonly IAvailabilityService _availabilityService;
-
-        public AvailabilityController(IAvailabilityService availabilityService)
-        {
-            _availabilityService = availabilityService;
-        }
-
-        [HttpPut("availabilities/{id}")]
-        public async Task<IActionResult> UpdateAvailability(int id, [FromBody] UpdateAvailabilityRequest request)
-        {
-            try
-            {
-                await _availabilityService.UpdateAvailability(id, request);
-
-                return Ok(new ApiResponse(StatusCodes.Status200OK, MessageConstants.SUCCESSFUL));
-            }
-            catch (Exception e)
-            {
-                throw new ServiceException(e.Message);
-            }
-        }
-
-        [HttpDelete("availabilities/{id}")]
-        public async Task<IActionResult> DeleteAvailability(int id)
-        {
-            try
-            {
-                await _availabilityService.DeleteAvailability(id);
-
-                return Ok(new ApiResponse(StatusCodes.Status200OK, MessageConstants.SUCCESSFUL));
-            }
-            catch (Exception e)
-            {
-                throw new ServiceException(e.Message);
-            }
-        }
-
-        [HttpPost("availability")]
-        public async Task<IActionResult> CreateAvailability([FromBody] CreateAvailabilityRequest request)
-        {
-            try
-            {
-                await _availabilityService.CreateAvailability(request);
-                return Ok(new ApiResponse(StatusCodes.Status200OK, MessageConstants.SUCCESSFUL, request));
-            }
-            catch (ServiceException e)
-            {
-                return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest, e.Message));
-            }
-        }
-
+        _availabilityService = availabilityService;
     }
+
+    [HttpPut("availabilities/{id}")]
+    public async Task<IActionResult> UpdateAvailability(int id, [FromBody] UpdateAvailabilityRequest request)
+    {
+        try
+        {
+            await _availabilityService.UpdateAvailability(id, request);
+
+            return Ok(new ApiResponse(StatusCodes.Status200OK, MessageConstants.SUCCESSFUL));
+        }
+        catch (Exception e)
+        {
+            throw new ServiceException(e.Message);
+        }
+    }
+
+    [HttpDelete("availabilities/{id}")]
+    public async Task<IActionResult> DeleteAvailability(int id)
+    {
+        try
+        {
+            await _availabilityService.DeleteAvailability(id);
+
+            return Ok(new ApiResponse(StatusCodes.Status200OK, MessageConstants.SUCCESSFUL));
+        }
+        catch (Exception e)
+        {
+            throw new ServiceException(e.Message);
+        }
+    }
+
+    [HttpPost("availability")]
+    public async Task<IActionResult> CreateAvailability([FromBody] CreateAvailabilityRequest request)
+    {
+        try
+        {
+            await _availabilityService.CreateAvailability(request);
+            return Ok(new ApiResponse(StatusCodes.Status200OK, MessageConstants.SUCCESSFUL, request));
+        }
+        catch (ServiceException e)
+        {
+            return BadRequest(new ApiResponse(StatusCodes.Status400BadRequest, e.Message));
+        }
+    }
+
 }
